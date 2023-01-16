650ccce1c9d7062e67e1dc4158f48611b88949e9
Saad Jawaid <33807729+RedFC@users.noreply.github.com>
2023-01-16 13:46:30 +0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -9776,20 +9776,25 @@ const core = __nccwpck_require__(4480);
 const github = __nccwpck_require__(4467);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const ArrayOfObjects = '[{"github": "RedFC", "slack_id": "U04167GDGJG", "name": "Saad jawaid"}]';
-  let parsingArray = JSON.parse(ArrayOfObjects)
+    // `who-to-greet` input defined in action metadata file
+    const ArrayOfObjects = '[{"github": "RedFC", "slack_id": "U04167GDGJG", "name": "Saad jawaid"}]';
+    let parsingArray = JSON.parse(ArrayOfObjects)
     // const ArrayOfObjects = core.getInput('who-to-greet');
-//   if()
-  console.log(`Hello ${parsingArray}!`);
-  console.log(`Hello ${github.context.actor}!`);
-  core.setOutput("slackid", parsingArray);
-  core.setOutput("actor", github.context.actor);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
+    let data = parsingArray.forEach(element => {
+        if(element.github == github.context.actor){
+           return element.slack_id
+        }
+    });
+    console.log(`Hello ${parsingArray}!`);
+    console.log(`Hello ${github.context.actor}!`);
+    console.log(`slack id ${data}!`);
+    core.setOutput("slackid", parsingArray);
+    core.setOutput("actor", github.context.actor);
+    //   // Get the JSON webhook payload for the event that triggered the workflow
+    //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //   console.log(`The event payload: ${payload}`);
 } catch (error) {
-//   core.setFailed(error.message);
+    //   core.setFailed(error.message);
 }
 })();
 
