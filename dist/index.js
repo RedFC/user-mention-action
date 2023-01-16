25b7c3e56358b812afb37024d78a44a10b919f55
Saad Jawaid <33807729+RedFC@users.noreply.github.com>
2023-01-16 13:49:38 +0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -9780,15 +9780,16 @@ try {
     const ArrayOfObjects = '[{"github": "RedFC", "slack_id": "U04167GDGJG", "name": "Saad jawaid"}]';
     let parsingArray = JSON.parse(ArrayOfObjects)
     // const ArrayOfObjects = core.getInput('who-to-greet');
-    let data = parsingArray.forEach(element => {
+    let data = parsingArray.map(element => {
         if(element.github == github.context.actor){
            return element.slack_id
         }
     });
+    
     console.log(`Hello ${parsingArray}!`);
     console.log(`Hello ${github.context.actor}!`);
     console.log(`slack id ${data}!`);
-    core.setOutput("slackid", parsingArray);
+    core.setOutput("slackid", data);
     core.setOutput("actor", github.context.actor);
     //   // Get the JSON webhook payload for the event that triggered the workflow
     //   const payload = JSON.stringify(github.context.payload, undefined, 2)
