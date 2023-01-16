879c8e38e167c5dd1253dd457949b30343f6cff2
Saad Jawaid <33807729+RedFC@users.noreply.github.com>
2023-01-16 14:31:40 +0500

    update action

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const github = require('@actions/github');
 
 try {
     // `who-to-greet` input defined in action metadata file
-    // const ArrayOfObjects = '[{"github": "RedFC", "slack_id": "U04167GDGJG", "name": "Saad jawaid"}]';
-    const ArrayOfObjects = core.getInput('who-to-mention-array');
+    const ArrayOfObjects = '[{"github": "RedFC", "slack_id": "U04167GDGJG", "name": "Saad jawaid"}]';
+    github.context.actor = "RedFC"
+    // const ArrayOfObjects = core.getInput('who-to-mention-array');
     let parsingArray = JSON.parse(ArrayOfObjects)
     let data = parsingArray.map(element => {
         if(element.github == github.context.actor){
            return element.slack_id
         }
     });
-    
-    console.log(`Hello ${parsingArray}!`);
-    console.log(`Hello ${github.context.actor}!`);
-    console.log(`slack id ${data}!`);
-    core.setOutput("slackid", data[0]);
+    let slackid = data[0].toString();
+    console.log(`Actor ${github.context.actor} !`);
+    console.log(`Slack Id `+slackid+' !');
+    core.setOutput("slackid", slackid);
     core.setOutput("actor", github.context.actor);
-    core.setOutput("footer",'mention action by saad jawaid aka @REDFC');
+    core.setOutput("footer",'user mention action by saad jawaid aka @REDFC');
     //   // Get the JSON webhook payload for the event that triggered the workflow
     //   const payload = JSON.stringify(github.context.payload, undefined, 2)
     //   console.log(`The event payload: ${payload}`);
